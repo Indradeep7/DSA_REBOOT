eda245b203ecea759ac40a01a577e125c6f3a566
Indradeep Chowdhury <indradeepchowdhury7@gmail.com>
2024-08-11 23:26:34 +0530

    selection_sort.cpp

--- sorting/buble_sort.cpp
+++ sorting/buble_sort.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 // selection sort
-// Selects the smallest (or largest) element from the unsorted part and swaps it with the first unsorted element.
+// Repeatedly swaps adjacent elements if they are in the wrong order.
 // Time Complexity: O(n²)
 // Space Complexity: O(1)
 
-void bubble_sort(int n,int arr[]);
-void bubble_sort(int n,int arr[]) {
+void bubble_sort(int n, int arr[]);
+void bubble_sort(int n, int arr[])
+{
+
     for (int i = 0; i < n - 1; i++)
     {
         int mini = i;
@@ -22,29 +24,27 @@ void bubble_sort(int n,int arr[]) {
 
 }
 
-
 int main()
 {
 
-    int arr[] = {13,46,24,52,20,9};
-    int n = (sizeof(arr)/sizeof(arr[0]));
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = (sizeof(arr) / sizeof(arr[0]));
 
     cout << "Before Sorting\n";
 
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     cout << endl;
     cout << endl;
 
-    bubble_sort(n,arr);
+    bubble_sort(n, arr);
 
     cout << "After Sorting\n";
 
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     cout << endl;
     cout << endl;
 
-    
-
-    
     return 0;
 }
