f02f9f630992a03f8900c0f61ad606b5d72f4407
Indradeep Chowdhury <indradeepchowdhury7@gmail.com>
2024-08-18 01:12:17 +0530

    quick_sort_comments

--- sorting/quick_sort/quick_sort.cpp
+++ sorting/quick_sort/quick_sort.cpp
@@ -1,92 +1,62 @@
-// #include <iostream>
-// #include <algorithm>
-// using namespace std;
-
-// int partition(int A[],int l,int h)  
-// {  
-//     int pivot=A[l];  
-//     int i=l,j=h;  
-//     do  
-//     {  
-//         do{i++;}while(A[i]<=pivot);  
-//         do{j--;}while(A[j]>pivot);  
-//     }while(i<j);  
-//     if(i<=j)swap(A[i],A[j]);  
-//     swap(A[l],A[j]);  
-//     return j;  
-// }
-// void QuickSort(int A[],int l,int h)  
-// {  
-//     int j;  
-//     if(l<h)  
-//     {  
-//         j=partition(A,l,h);  
-//         QuickSort(A,l,j);  
-//         QuickSort(A,j+1,h);  
-//     }  
-// }  
-// int main()  
-// {  
-//     int A[]={11,13,7,12,16,9,24,5,10,3,INT32_MAX},n=11,i;  
-//     QuickSort(A,0,n-1);  
-//     for(i=0;i<10;i++)  
-//         printf("%d ",A[i]);  
-//     printf("\n");  
-//     return 0;  
-// }  
-
 #include <iostream>
 #include <algorithm> // For swap
 
 using namespace std;
 
+// Function to partition the array around a pivot element
 int partition(int A[], int l, int h)
 {
-    int pivot = A[l];
+    int pivot = A[l]; // Choosing the first element as the pivot
     int i = l, j = h;
 
     do
     {
-        do{i++;} while (A[i] <= pivot);
-        do{j--; } while (A[j] > pivot);
+        // Increment i until an element greater than pivot is found
+        do { i++; } while (A[i] <= pivot);
+        // Decrement j until an element less than or equal to pivot is found
+        do { j--; } while (A[j] > pivot);
 
-        if (i < j)swap(A[i], A[j]);
+        // Swap elements if i is less than j
+        if (i < j) swap(A[i], A[j]);
     } while (i < j);
 
+    // Place the pivot element in its correct position
     swap(A[l], A[j]);
-    return j;
+    return j; // Return the index of the pivot
 }
 
+// Recursive QuickSort function
 void QuickSort(int A[], int l, int h)
 {
     if (l < h)
     {
+        // Partition the array and get the pivot index
         int j = partition(A, l, h);
+        // Recursively sort the elements before the pivot
         QuickSort(A, l, j);
+        // Recursively sort the elements after the pivot
         QuickSort(A, j + 1, h);
     }
 }
 
 int main()
 {
-    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3};
-    int n = sizeof(A) / sizeof(A[0]);
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}; // Array to be sorted
+    int n = sizeof(A) / sizeof(A[0]); // Calculate the number of elements in the array
 
     cout << "Before Sorting\n";
-
     for (int i = 0; i < n; i++)
-        cout << A[i] << " ";
+        cout << A[i] << " "; // Print the array before sorting
     cout << endl;
     cout << endl;
 
-    QuickSort(A, 0, n);
+    QuickSort(A, 0, n); // Call QuickSort to sort the array
 
     cout << "After Sorting\n";
-
     for (int i = 0; i < n; i++)
-        cout << A[i] << " ";
+        cout << A[i] << " "; // Print the array after sorting
     cout << endl;
     cout << endl;
 
-    return 0;
+    return 0; // Return success
 }
